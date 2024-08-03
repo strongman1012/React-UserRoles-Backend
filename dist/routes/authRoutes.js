1a664d4869bc4aafd2be4f6c3d2903c055e88cab
strongman1012 <alexander.schvetc.2000@gmail.com>
2024-08-03 16:10:31 +0300

    user roles logout fix

--- dist/routes/authRoutes.js
+++ dist/routes/authRoutes.js
@@ -3,11 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const authController_1 = require("../controllers/authController");
 const authenticate_1 = require("../middlewares/authenticate");
-const authenticate_2 = require("../middlewares/authenticate");
 const router = (0, express_1.Router)();
 router.post('/login', authenticate_1.authenticate, authController_1.login);
 router.post('/azure-login', authenticate_1.azureAuthenticate, authController_1.azureAdLogin);
 router.post('/register', authController_1.register);
-router.post('/logout', authenticate_2.checkJwt, authController_1.logout);
+router.post('/logout', authController_1.logout);
 exports.default = router;
 //# sourceMappingURL=authRoutes.js.map
--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 import { login, register, logout, azureAdLogin } from '../controllers/authController';
 import { authenticate, azureAuthenticate } from '../middlewares/authenticate';
-import { checkJwt } from '../middlewares/authenticate';
 
 const router = Router();
 
 router.post('/login', authenticate, login);
 router.post('/azure-login', azureAuthenticate, azureAdLogin);
 router.post('/register', register);
-router.post('/logout', checkJwt, logout);
+router.post('/logout', logout);
 
 export default router;
